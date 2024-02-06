7157fc13cd19a9b0d224bada988894fad9708a03
Italo Peruchi <itl95pps@gmail.com>
2024-02-06 09:03:18 -0300

    Build

--- lib/config.d.ts
+++ lib/config.d.ts
@@ -1,10 +1,12 @@
 /// <reference types="node" />
 /// <reference types="node" />
+/// <reference types="node/http" />
+/// <reference types="got-cjs/dist/source/core/timed-out" />
 import { Agent as HttpsAgent } from 'https';
 import { Agent as HttpAgent } from 'http';
 export interface HLTVConfig {
     loadPage: (url: string) => Promise<string>;
     httpAgent: HttpsAgent | HttpAgent;
 }
-export declare const defaultLoadPage: (httpAgent: HttpsAgent | HttpAgent | undefined) => (url: string) => any;
+export declare const defaultLoadPage: (httpAgent: HttpsAgent | HttpAgent | undefined) => (url: string) => Promise<any>;
 export declare const defaultConfig: HLTVConfig;
